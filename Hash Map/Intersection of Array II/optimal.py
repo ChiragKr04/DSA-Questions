7472eb4bb06228c47ad31aedc0531e26df76c3b0
chiragkr04 <chiragkumar6921@gmail.com>
2022-06-28 23:01:29 +0530

    2 array intersection 2

---
+++ Hash Map/Intersection of Array II/optimal.py
@@ -0,0 +1,15 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        res = []
+
+        map = Counter(nums1)
+
+        for x in nums2:
+
+            if x in map and map[x] > 0:
+
+                res.append(x)
+                map[x] -= 1
+
+        return res
