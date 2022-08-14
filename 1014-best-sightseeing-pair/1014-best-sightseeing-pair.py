778e431a79023edc4325552d61a67283be2a58a5
Chirag Bargoojar <chiragkumar6921@gmail.com>
2022-08-14 21:27:04 +0530

    Time: 862 ms (32.52%), Space: 20.6 MB (66.74%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        
+        le = len(values)
+        larr = [0]*le
+        rarr = [0]*le
+        
+        m = 0
+        
+        for i in range(le-1):
+            rarr[i] = max(values[i]+i,rarr[i-1])
+        
+        
+        larr[le-1] = values[le-1] - (le-1)
+        
+        for i in range(le-2,-1,-1):
+            larr[i] = max(values[i]-i,larr[i+1])
+            
+        # print(larr,rarr)
+            
+        
+        for i in range(le-1):
+            
+            m = max(rarr[i] + larr[i+1],m)
+            
+
+        return m
+            
+        
+        
