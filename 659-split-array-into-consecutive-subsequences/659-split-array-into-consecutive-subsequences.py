90c1856bbd67b2eebd1752f0b64857554cfe8fe1
Chirag Bargoojar <chiragkumar6921@gmail.com>
2022-08-20 00:31:33 +0530

    Time: 1012 ms (26.45%), Space: 15.4 MB (25.22%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isPossible(self, nums: List[int]) -> bool:
+        
+        
+        freq = Counter(nums)
+        start, end = nums[0], nums[len(nums)-1]
+        i = start
+        while i < end+1:
+            if freq[i] == 0:
+                i+=1
+                continue
+
+            max_freq = freq[i]
+            j=i
+            count=0
+            while j < end+1 and freq[j] >= max_freq:
+                if max_freq < freq[j]:
+                    max_freq = freq[j]
+
+                freq[j]-=1
+                j+=1
+                count+=1
+            if count < 3:
+                return False
+        return True
+            
+        
+        
+        
