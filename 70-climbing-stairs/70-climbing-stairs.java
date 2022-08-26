cdd52763094a058c6a96e4440534d484a005281a
Chirag Bargoojar <chiragkumar6921@gmail.com>
2022-08-26 21:33:00 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (68.23%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        
+        if(n == 1){
+            return 1;
+        }
+        
+        if(n == 2){
+            return 2;
+        }
+        
+        int sum = 0;
+        
+        int n1 = 1;
+        int n2 = 2;
+        
+        for(int i = 3; i <= n; i++){
+            
+            sum = n1 + n2;
+            n1 = n2;
+            n2 = sum;
+            
+        }
+        
+        
+        return sum;
+        
+    }
+}
