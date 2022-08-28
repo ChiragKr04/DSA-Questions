9f8b1e2680e3ad8900c4cdd65db6a002460bba97
Chirag Bargoojar <chiragkumar6921@gmail.com>
2022-08-28 11:29:16 +0530

    Time: 1851 ms (64.21%), Space: 21.6 MB (28.42%) - LeetHub

---
+++ 1664-ways-to-make-a-fair-array/1664-ways-to-make-a-fair-array.py
@@ -0,0 +1,46 @@
+class Solution:
+    def waysToMakeFair(self, nums: List[int]) -> int:
+        
+        l = len(nums)
+        
+        odd = list([0]*l)
+        even = list([0]*l)
+        
+        osum = 0
+        esum = 0
+        
+        for i in range(0,l):
+            
+            if i % 2 == 0:
+                
+                esum += nums[i]
+            
+            else:
+                
+                osum += nums[i]
+                
+            odd[i] = osum
+            even[i] = esum
+        
+        ans = 0
+        
+        for i in range(0, l):
+            
+            if i == 0:
+                nos = even[l-1] - nums[0]
+                nes = odd[l-1]
+                if nes == nos:
+                    ans+=1
+                continue
+            
+            nes = even[i-1] + odd[l-1] - odd[i]
+            nos = odd[i-1] + even[l-1] - even[i]
+            
+            if nes == nos:
+                ans+=1
+                    
+        return ans
+        
+        
+        
+        
