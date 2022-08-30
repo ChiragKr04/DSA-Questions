b772def899d72efdecaf197e205a0213d3b105d6
Chirag Bargoojar <chiragkumar6921@gmail.com>
2022-08-30 12:03:28 +0530

    Time: 346 ms (59.79%), Space: 15.5 MB (73.24%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -22,22 +22,3 @@ class Solution:
                 
         return -1
         
-        # while(low <= high){
-        #     int mid = (int)((low+high)/2);
-        #     if(nums[mid]==target){
-        #         return mid;
-        #     }
-        #     if(nums[mid] > target){
-        #         high = mid - 1;
-        #     }
-        #     else if (nums[mid] < target){
-        #         low = mid + 1;
-        #     }
-        # }
-        # return -1;
-        
-        
-        
-        
-        
-        
