bfc1605144458f939cb1ef65cc6be1b0675f5987
Chirag Bargoojar <chiragkumar6921@gmail.com>
2022-09-03 13:29:24 +0530

    Time: 786 ms (59.90%), Space: 55.2 MB (5.15%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
@@ -9,7 +9,7 @@ class Solution:
         
         def dfs(root):
             if not root:
-                return float("inf")
+                return inf
             
             if not root.left and not root.right:
                 return 1
