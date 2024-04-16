e1d403cb788625de463bb4b1c15b4e4bdfe13fbf
Chirag Bargoojar <chiragkumar6921@gmail.com>
2024-04-16 23:20:07 +0530

    Time: 42 ms (83.50%), Space: 17.7 MB (73.75%) - LeetHub

--- 0623-add-one-row-to-tree/0623-add-one-row-to-tree.py
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.py
@@ -10,16 +10,30 @@ class Solution:
         if depth == 1:
             return TreeNode(val, root, None)
         
-        elif depth == 2:
-            root.left = TreeNode(val, root.left, None)
-            root.right = TreeNode(val, None, root.right)
-
-        else:
-            if root.left:
-                self.addOneRow(root.left, val, depth - 1)
-            if root.right:
-                self.addOneRow(root.right, val, depth - 1)
+        queue = [root]
         
+        while 0 < len(queue):
+            
+            depth -= 1
+            
+            count = len(queue)
+            
+            for _ in range(count):
+                node = queue.pop(0)
+                
+                if node.left is not None:
+                    queue.append(node.left)
+                    
+                if node.right is not None:
+                    queue.append(node.right)
+                    
+                if depth == 1:
+                    node.left = TreeNode(val, node.left, None)
+                    node.right = TreeNode(val, None, node.right)
+                
+            if depth == 1:
+                break
+                
         return root
 
 
