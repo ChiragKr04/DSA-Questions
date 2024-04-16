b541756c22e77571d24da3df09cbd6c366ff1d98
Chirag Bargoojar <chiragkumar6921@gmail.com>
2024-04-16 23:22:40 +0530

    dfs solution

--- 0623-add-one-row-to-tree/0623-add-one-row-to-tree.py
+++ 0623-add-one-row-to-tree/0623-add-one-row-to-tree.py
@@ -36,5 +36,21 @@ class Solution:
                 
         return root
 
+# class Solution:
+#     def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        
+#         if depth == 1:
+#             return TreeNode(val, root, None)
+        
+#         elif depth == 2:
+#             root.left = TreeNode(val, root.left, None)
+#             root.right = TreeNode(val, None, root.right)
 
+#         else:
+#             if root.left:
+#                 self.addOneRow(root.left, val, depth - 1)
+#             if root.right:
+#                 self.addOneRow(root.right, val, depth - 1)
+        
+#         return root
 
