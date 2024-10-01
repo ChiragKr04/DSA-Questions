4cd47442c9e6394fd2022bc141b9aba2569a21d5
Chirag Bargoojar <chiragkumar6921@gmail.com>
2024-10-01 23:19:30 +0530

    Time: 102 ms (75.65%), Space: 17.7 MB (43.24%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+
+        #  2 4 1 1 1 1 1
+        
+        myjumps = 0
+        curMaxDist = 0
+        end = 0
+        
+        print(len(nums))
+        
+        for i in range(len(nums) - 1):
+            
+            curMaxDist = max(curMaxDist, i + nums[i])
+            
+            if i == end:
+                myjumps += 1
+                
+                end = curMaxDist
+            
+            
+        
+        return myjumps
+            
+            
+        
+        
+
+
+        
