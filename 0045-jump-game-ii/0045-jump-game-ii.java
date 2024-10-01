5881111b628f04e20860e6a16e9cf9508d929cd4
Chirag Bargoojar <chiragkumar6921@gmail.com>
2024-10-01 23:23:29 +0530

    Time: 1 ms (98.28%), Space: 45 MB (63.88%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        int myjumps = 0;
+        int curMaxDist = 0;
+        int end = 0;
+                
+        for(int i = 0; i < nums.length - 1; i++){
+            curMaxDist = Math.max(curMaxDist, i + nums[i]);
+            if (i == end){
+                 myjumps += 1;
+                
+                end = curMaxDist;
+            }
+               
+        }
+        
+        return myjumps;
+    }
+}
