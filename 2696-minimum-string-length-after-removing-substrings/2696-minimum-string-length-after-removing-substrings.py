d3bc79500e32b1265b7c3cd0b6d2567eb5901180
Chirag Bargoojar <chiragkumar6921@gmail.com>
2024-10-07 22:38:24 +0530

    Time: 48 ms (34.19%), Space: 16.6 MB (41.77%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minLength(self, s: str) -> int:
+        
+        stack = []
+        
+        for char in s:
+            
+            if len(stack) == 0:
+                stack.append(char)
+                continue
+                
+            if char == "B" and stack[-1] == "A":
+                stack.pop()
+                
+            elif char == "D" and stack[-1] == "C":
+                stack.pop()
+                
+            else:
+                stack.append(char)
+                
+        
+        return len(stack)
+            
+            
+        
+        
+        
