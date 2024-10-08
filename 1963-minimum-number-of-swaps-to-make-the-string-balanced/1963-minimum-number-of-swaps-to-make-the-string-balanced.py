4de70bcee5a9bbae6cbb57acf5072876733a3bd5
Chirag Bargoojar <chiragkumar6921@gmail.com>
2024-10-08 22:29:41 +0530

    Time: 187 ms (76.77%), Space: 27.9 MB (82.95%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -1,21 +1,13 @@
 class Solution:
     def minSwaps(self, s: str) -> int:
         
-        isOpen = '['
-        isClose = ']'
-        
         swaps = 0
         
         p1 = 0
-        
-        
-        if len(s) == 2 and s[0] == isOpen and s[1] == isClose:
-            return 0
-        
-        
+
         for char in s:
             
-            if char == isOpen:
+            if char == '[':
                 
                 p1 += 1
                 
