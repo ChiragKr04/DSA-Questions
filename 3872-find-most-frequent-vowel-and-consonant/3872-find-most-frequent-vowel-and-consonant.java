8bef4783694ea53517de44ab310f97d3756cbe0f
Chirag Bargoojar <chiragkumar6921@gmail.com>
2025-09-13 09:02:39 +0530

    Time: 1 ms (100%), Space: 42.2 MB (96.46%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -1,42 +1,22 @@
 class Solution {
     public int maxFreqSum(String s) {
 
-        HashMap<Character, Integer> vow = new HashMap<>();
-        HashMap<Character, Integer> con = new HashMap<>();
+        int arr[] = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            arr[c - 'a']++;
+        }
+        int vmax = 0;
+        int cmax = 0;
+        for (int i = 0; i < arr.length; i++) {
 
-        for (char c : s.toCharArray()) {
-
-            if (isVowel(c)) {
-                vow.put(c, vow.getOrDefault(c, 0) + 1);
-            } else {
-                con.put(c, con.getOrDefault(c, 0) + 1);
+            if ((i == 0 || i == 4 || i == 8 || i == 14 || i == 20) && arr[i] > vmax) {
+                vmax = arr[i];
+            } else if ((i != 0 && i != 4 && i != 8 && i != 14 && i != 20) && arr[i] > cmax) {
+                cmax = arr[i];
             }
-
         }
-
-        Integer vmax = 0;
-        Integer cmax = 0;
-
-        for (Character key : vow.keySet()) {
-            vmax = Math.max(vmax, vow.get(key));
-        }
-
-        for (Character key : con.keySet()) {
-            cmax = Math.max(cmax, con.get(key));
-        }
-
         return vmax + cmax;
-
-    }
-
-    public boolean isVowel(char c) {
-
-        switch (c) {
-            case 'a', 'e', 'i', 'o', 'u':
-                return true;
-        }
-
-        return false;
 
     }
 
