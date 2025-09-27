8468390ad18a45dffc7376a9ff0dc554d976acc7
Chirag Bargoojar <chiragkumar6921@gmail.com>
2025-09-27 10:44:46 +0530

    Time: 118 ms (17.01%), Space: 64.8 MB (5.46%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,38 +1,33 @@
 class Solution {
     public int countPrimes(int n) {
-        if (n < 3) return 0;
 
-        // We only store odd numbers (save ~50% memory)
-        int size = (n >> 1); // count of odd numbers < n
-        byte[] isPrime = new byte[(size + 7) >> 3]; // bit array
+        if(n < 2) return 0;
+        
+        int [] isPrime = new int[n+1];
 
-        // Initially, assume all odd numbers are prime
-        for (int i = 0; i < isPrime.length; i++) {
-            isPrime[i] = (byte) 0xFF;
+        for(int i=2;i<=n;i++){
+            isPrime[i] = 1;
         }
+        isPrime[0] = isPrime[1] = 0;
 
-        int limit = (int)Math.sqrt(n);
-        for (int i = 3; i <= limit; i += 2) {
-            if (!getBit(isPrime, i >> 1)) continue;
-            // mark multiples of i as not prime
-            for (int j = i * i; j < n; j += i << 1) { // skip even multiples
-                clearBit(isPrime, j >> 1);
+
+        for(int i = 2; i * i <= n; i++) {
+
+            if(isPrime[i] == 1) {
+                for(int j = i*i; j<=n; j+=i) {
+                    isPrime[j] = 0;
+                }
             }
+
         }
 
-        // count primes (2 + all odd primes)
-        int count = 1; // for prime number 2
-        for (int i = 1; i < size; i++) {
-            if (getBit(isPrime, i)) count++;
+        int ct = 0;
+        for(int i=2; i<n; i++) {
+            if(isPrime[i] == 1) ct++;
         }
-        return count;
-    }
 
-    private boolean getBit(byte[] arr, int idx) {
-        return (arr[idx >> 3] & (1 << (idx & 7))) != 0;
-    }
 
-    private void clearBit(byte[] arr, int idx) {
-        arr[idx >> 3] &= ~(1 << (idx & 7));
+        return ct;
+
     }
 }
