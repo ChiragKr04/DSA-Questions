e8f2b6e1a5bce57fdc5747703f8839b7b8e97a58
Chirag Bargoojar <chiragkumar6921@gmail.com>
2025-10-02 10:00:30 +0530

    Time: 1 ms (30.32%), Space: 41.2 MB (38.06%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+
+        int fullBottle = numBottles;
+        int emptyBottles = 0;
+        int exchangeRate = numExchange;
+        int bottleDrunk = 0;
+
+        while(true) {
+            if(fullBottle == numBottles) {
+                emptyBottles += fullBottle;
+                bottleDrunk += emptyBottles;
+                fullBottle = 0;
+                continue;
+            }
+            if(emptyBottles >= exchangeRate) {
+                fullBottle++;
+                emptyBottles = emptyBottles - exchangeRate;
+                exchangeRate++;
+            } else if (fullBottle > 0) {
+                emptyBottles += fullBottle;
+                bottleDrunk += fullBottle;
+                fullBottle = 0;
+            } else {
+                return bottleDrunk;
+            }
+        }
+
+        // return bottleDrunk;
+        
+    }
+}
