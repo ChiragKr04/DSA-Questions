e62c79f808548f83d9a8d6c6c4e1e36ecab5e6f8
Chirag Bargoojar <chiragkumar6921@gmail.com>
2025-10-11 10:43:25 +0530

    Time: 53 ms (100%), Space: 61 MB (85.58%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
@@ -1,39 +1,26 @@
 class Solution {
     public long maximumTotalDamage(int[] power) {
-        // 1. Count the occurrences of each damage value
-        Map<Integer, Integer> countMap = new HashMap<>();
-        for (int damage : power) {
-            countMap.put(damage, countMap.getOrDefault(damage, 0) + 1);
-        }
+        Arrays.sort(power);
 
-        // 2. Get sorted unique damages
-        List<Integer> uniqueDamages = new ArrayList<>(countMap.keySet());
-        Collections.sort(uniqueDamages);
-
-        int n = uniqueDamages.size();
-        long[] dp = new long[n + 1];
-
-        // 3. Dynamic Programming
-        for (int i = 0; i < n; i++) {
-            int currentDamage = uniqueDamages.get(i);
-            long currentDamageTotal = (long) currentDamage * countMap.get(currentDamage);
-
-            // Find the first index 'j' such that uniqueDamages[j-1] < currentDamage - 2
-            int j = i;
-            while (j > 0 && uniqueDamages.get(j - 1) >= currentDamage - 2) {
-                j--;
+        long[] dp = new long[power.length];
+        long md = 0;
+        dp[0] = power[0];
+        for (int i = 1, j = 0; i < power.length; i++) {
+            if (power[i] == power[i-1]) {
+                dp[i] = dp[i-1]+power[i];
+            } else {
+                while (power[j]+2 < power[i]) {
+                    md = Math.max(md, dp[j]);
+                    j++;
+                }
+                dp[i] = md + power[i];
             }
-
-            // Calculate dp[i+1] based on two choices: skipping or taking
-            // Skip: max damage is the same as the previous step
-            long skipDamage = dp[i];
-            // Take: damage from current spell + max damage from a compatible previous step (dp[j])
-            long takeDamage = dp[j] + currentDamageTotal;
-
-            dp[i + 1] = Math.max(skipDamage, takeDamage);
         }
-
-        // 4. The final result is the last element of the dp array
-        return dp[n];
+        
+        long max = 0;
+        for (long n : dp) {
+            max = Math.max(max, n);
+        }
+        return max;
     }
 }
