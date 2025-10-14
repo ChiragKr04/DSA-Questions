36dfbbe16fa50a45e96537506a96f94bd0155278
Chirag Bargoojar <chiragkumar6921@gmail.com>
2025-10-14 08:42:17 +0530

    Time: 1 ms (100%), Space: 44.5 MB (50%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
@@ -3,8 +3,8 @@ class Solution {
         int knew = k - 1;
         if (knew == 0)
             return true;
-        for (int j = k + 1; j < nums.size(); j++) {
-            if (nums.get(j) > nums.get(j - 1) && nums.get(j - k) > nums.get(j - k - 1)) {
+        for (int i = k + 1; i < nums.size(); i++) {
+            if (nums.get(i) > nums.get(i - 1) && nums.get(i - k) > nums.get(i - k - 1)) {
                 knew--;
             } else {
                 knew = k - 1;
